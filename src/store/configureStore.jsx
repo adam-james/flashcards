8d43a6e63f70883ae1dcf29d2179b01c2bd3ed52
adam-james <adam.j.sulewski@gmail.com>
2016-07-20 16:54:57 -0700

    configure to work with devTools extension

--- src/store/configureStore.jsx
+++ src/store/configureStore.jsx
@@ -1,10 +1,22 @@
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 
 import rootReducer from '../reducers';
 
 
+let finalCreateStore;
+
+if (process.env.NODE_EN === 'production') {
+  finalCreateStore = compose()(createStore);
+}
+else {
+  finalCreateStore = compose(
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  )(createStore);
+}
+
+
 export default (initialState) => {
-  const store = createStore(rootReducer, initialState);
+  const store = finalCreateStore(rootReducer, initialState);
 
   if (module.hot) {
     module.hot.accept('../reducers', () => {
